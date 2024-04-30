57a6e948536aacd9c57c36123b1c92d022116385
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-04-29 21:39:10 -0400

    Started basic framework for Interface and Store

--- src/Interface.java
+++ src/Interface.java
@@ -1,3 +1,33 @@
-class Interface {
+import java.util.Scanner;
 
-}
+public class Interface {
+
+    public static void main (String args[]) {
+       
+        if (args.length == 0) {
+            System.out.println("Usage: Interface storeName.store");
+            System.exit(1);
+        }
+
+       String filename = args[0];
+       Store s = new Store(filename);
+
+       Scanner stdin = new Scanner(System.in);
+       boolean complete = false;
+       //TODO
+       // System.out.println(s.getStores()); (for store names)
+
+       while (!complete) {
+           System.out.println("For help type, \"help\"");
+           String input = stdin.nextLine();
+            if (input.equals("exit")) {
+                complete = true;
+            } else {
+                //TODO
+                System.out.println();
+                //Action a = ActionFactory.instance().parse(input);
+                //System.out.println(a.execute());
+            }
+       } 
+    } // end main
+} // end class
--- src/Store.java
+++ src/Store.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Hashtable;
 class Store {
+
+    private String name;
+    private String location;
+    private ArrayList<Item> items;
+    private Hashtable<String, Integer> inven;
+
+    public Store (String filename) {
+        this.name = "tmp";
+        this.location = "tmp";
+        this.items = new ArrayList<Item>();
+        this.inven = new Hashtable<String, Integer>();
+    }
 
 }
