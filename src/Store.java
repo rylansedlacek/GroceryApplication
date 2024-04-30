21ca5290f3f743b909d45653cc5635380c382f61
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-04-29 22:27:03 -0400

    Started constructor

--- src/Store.java
+++ src/Store.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.FileReader;
 class Store {
 
     private String name;
     private String location;
     private ArrayList<Item> items;
-    private Hashtable<String, Integer> inven;
+    private Hashtable<String, Double> inven;
 
     public Store (String filename) {
-        this.name = "tmp";
-        this.location = "tmp";
+        this.name = "";
+        this.location = "";
         this.items = new ArrayList<Item>();
         this.inven = new Hashtable<String, Integer>();
+
+        Scanner s = new Scanner(new FileReader(filename);
+        this.name = s.nextLine();
+        this.location = s.nextLine();
+
+        try  {
+            while (true) {
+            Item i = new Item(s);
+            Store.addItem(i);
+            Store.addInven(i.getName,i.getPrice);
+            }
+        } catch (NoItemException e) {
+        }
+
     }
 
 }
