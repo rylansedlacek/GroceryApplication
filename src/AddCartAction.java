3d9b0c36f13f082688f63c34593b4f5d7c3fe6ee
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-05-21 20:37:49 -0400

    Prettied up, started on cart logic:wq

---
+++ src/AddCartAction.java
@@ -0,0 +1,36 @@
+import java.util.Hashtable;
+import java.util.ArrayList;
+
+class AddCartAction extends Action {
+
+    private Store currentStore;
+    private Item theItem;
+
+    public class AddCartAction(Store curr, Item item) {
+        this.currentStore = curr;
+        this.theItem = item;
+
+    }
+
+
+    String execute() {
+
+        String retVal = "";
+        Hashtable<Store, Item> currentCart = Background.instance().getFullCart();
+        
+
+        //TODO : ADD A STOCK DECREMENTQ
+
+
+        //if DUPLICATE, check stock
+        //add if able
+
+        //basic easy stuff
+
+
+        return "AddCART IN PROGRESS";
+    }
+
+
+
+}
--- src/Background.java
+++ src/Background.java
@@ -1,11 +1,17 @@
+import java.util.Hashtable;
+import java.util.ArrayList;
 class Background {
 
     private StoreManager theManager;
     private static Background theInstance;
+    private Hashtable<Store,Item> userCart;
+    private ArrayList<Item> itemCart;
 
     public Background() {
         this.theInstance = theInstance;
         this.theManager = theManager;
+        this.userCart = new Hashtable<Store,Item>();
+        this.itemCart = new ArrayList<Item>();
     }
 
     public static synchronized Background instance() {
@@ -23,6 +29,20 @@ class Background {
         return this.theManager;
     }
 
+    public void addToCart(Store currStore, Item item) {
+        this.userCart.put(currStore,item);
+    }
 
+    public void addToItem(Item item) {
+        this.itemCart.add(item);
+    }
+
+    public Hashtable getFullCart() {
+        return this.userCart;
+    }
+
+    public ArrayList getItemCart() {
+        return this.itemCart;
+    }
 
 }
