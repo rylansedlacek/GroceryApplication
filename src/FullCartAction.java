4ae3282d7909debe58e01f0df666b5bd8899aa9d
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-05-27 12:49:28 -0400

    Version 1.1 complete, will pretty up tuesday

--- src/FullCartAction.java
+++ src/FullCartAction.java
@@ -5,11 +5,32 @@ class FullCartAction extends Action {
     
 
     String execute() {
-        return "TMP FULL IN PROG";
+        String retVal = "";
+        
+        ArrayList<Store> visitedStores = Background.instance().getVisitedStores();
+        Hashtable<Item, Integer> quant = Background.instance().getItemQuantCart();
+
+        if (visitedStores == null || visitedStores.isEmpty()) {
+            return "|EMPTY CART|" + "\n";
+        }
+
+       for (int i=0; i<visitedStores.size(); i++) {
+          Store tmp = visitedStores.get(i);
+          ArrayList<Item> tmpItems = Background.instance().getMainCart(tmp);
+            
+          retVal += "\n" + tmp.getName() + "\n";
+          retVal += "-----------------------" + "\n";
+             for (int j=0; j<tmpItems.size(); j++) {
+                retVal +=  "NAME: " + tmpItems.get(j).getName() + "\n";
+                retVal += "QUANTITY: " + quant.get(tmpItems.get(j)) + "\n";
+                retVal += "\n";
+             }
+            tmp = null;
+            tmpItems = new ArrayList<Item>();
+       }
+                
+       return retVal;
 
     }
-
-
-
 
 }
